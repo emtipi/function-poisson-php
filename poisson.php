92176e3f7d523141e3b0104cd370cd5f03685376
emtipi <74175959+emtipi@users.noreply.github.com>
2020-11-09 12:15:34 +0100

    Update poisson.php
    Update code

--- poisson.php
+++ poisson.php
@@ -1,26 +1,25 @@
-define('exponential', 2.71828182845904523536028747135266249775724709369995957496696762772407663035354759457138217852516642742746); 
-
-$numerator = 0; 
-$dominator = 0; 
- function fact($x) {
-        if($x==0) {
-            return 1;
-        }
-        return $x * fact($x-1);
-        }
+<?php
+function fact($x) {
+    if($x==0) {
+        return 1;
+    }
+    return $x * fact($x-1);
+    }
 
 
-        function poisson($k, $landa)
+    function poisson($k, $landa)
 {
-        $exponential = 2;
-        $exponentialPower = pow($exponential, -$landa);
-        $landaPowerK = pow($landa,$k);
-        $numerator = $exponentialPower * $landaPowerK;
+    define('exponential', 2.71828182845904523536028747135266249775724709369995957496696762772407663035354759457138217852516642742746);
+    $exponentialPower = pow($exponential, -$landa);
+    $landaPowerK = pow($landa,$k);
+    $numerator = $exponentialPower * $landaPowerK;
 
-        $dominator = fact($k);
+    $dominator = fact($k);
 
-        echo ($numerator / $dominator);
+    echo ($numerator / $dominator);
 }
 
 //Test formule:
 poisson(1,2);
+
+?>
